dd0ba2174303f0a1087870bbb1f16db792583cc8
lionelritchie29 <lionelrtchieee@gmail.com>
2023-05-23 18:16:39 +0700

    feat: view, close, and visit detail video for overlay

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ window.addEventListener('DOMContentLoaded', () => {
   const resultCardTemplate = document.getElementById('result-card-template');
   const originalSearchResultContainer = document.getElementById('search-results');
   const overlayElement = document.getElementById('view-overlay');
+  const closeOverlayBtn = document.getElementById('view-overlay-close');
+  const overlayIframeElm = document.getElementById('view-overlay-src');
+  const overlayVisitBtn = document.getElementById('view-overlay-visit');
 
   let currentResults = [];
+  let activeOverlayLink = '';
 
   const onSearchResultReadyCallback = (name, q, promos, results, resultsDiv) => {
     console.log({ results });
@@ -29,9 +33,21 @@ window.addEventListener('DOMContentLoaded', () => {
         resultCard.getElementById('result-card-view-count').innerText =
           currentResults[index].richSnippet.videoobject.interactioncount;
       }
+      resultCard.getElementById('result-card').setAttribute('data-index', index);
+
+      resultCard.getElementById('result-card').addEventListener('click', function (e) {
+        overlayElement.style.display = 'flex';
+
+        const dataIdx = e.target.offsetParent.getAttribute('data-index');
+        if (currentResults[dataIdx].richSnippet.videoobject) {
+          overlayIframeElm.src = currentResults[dataIdx].richSnippet.videoobject.embedurl;
+          activeOverlayLink = currentResults[dataIdx].richSnippet.videoobject.url;
+        }
+      });
 
       resultElm.innerHTML = '';
       resultElm.appendChild(resultCard);
+
       index++;
     }
   };
@@ -39,6 +55,18 @@ window.addEventListener('DOMContentLoaded', () => {
   const onStartSearchCallback = (gname, query) => {
     return `${query}`;
   };
+
+  const onOverlayClose = () => {
+    overlayElement.style.display = 'none';
+  };
+
+  const onOverlayVisit = () => {
+    overlayElement.style.display = 'none';
+    overlayVisitBtn.parentElement.href = activeOverlayLink;
+  };
+
+  closeOverlayBtn.addEventListener('click', onOverlayClose);
+  overlayVisitBtn.addEventListener('click', onOverlayVisit);
 
   window.__gcse || (window.__gcse = {});
   // (window.__gcse.parsetags = 'explicit'),
